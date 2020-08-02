73d113d42e9b3fa7821b7844c7f4a745ac2b62c2
Adriano Takeshi Wakavaiachi <38956649+adrianotakeshi@users.noreply.github.com>
2020-08-02 19:14:13 -0300

    Update index.js

--- src/page/cadastro/Categoria/index.js
+++ src/page/cadastro/Categoria/index.js
@@ -19,8 +19,8 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://devsoutinhoflix.herokuapp.com/categorias';
-    // E a ju ama variáveis
+      : 'https://pokeflix.herokuapp.com/categorias';
+   
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
